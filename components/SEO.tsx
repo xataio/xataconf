ad939b4b56391084c93884007c95668462d44ccc
Tejas Kumar <tejas@tejas.qa>
2022-07-13 01:22:29 +0200

    Remove theme-color wtf

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -61,7 +61,6 @@ export const SEO = () => {
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#da532c" />
-      <meta name="theme-color" content="#ffffff" />
     </Head>
   )
 }
