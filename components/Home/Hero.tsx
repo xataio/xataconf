3ecdd1c8ae51597b3ed58dd0c8d9af331890bfa2
Tejas Kumar <tejas@tejas.qa>
2022-07-12 16:30:12 +0200

    Fix height

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -20,31 +20,37 @@ export const Hero = () => {
       }}
     >
       <div
+        className="flex flex-col"
         style={{
           height: "calc(100vh - 56px)",
         }}
-        className="max-w-[1280px] mx-auto flex flex-col items-center justify-center gap-16 p-4 leading-none text-center"
       >
-        <video
-          autoPlay
-          onLoadStart={(e) => {
-            e.currentTarget.playbackRate = 1.5
-          }}
-          playsInline
-          muted
-          controls={false}
-          loop={false}
-          className="h-40 md:h-80"
-        >
-          <source src="/hero/fly-in.mov" type='video/mp4; codecs="hvc1"' />
-          <source src="/hero/fly-in.webm" type='video/webm; codecs="vp8"' />
-        </video>
-        <H1>
-          A free online event celebrating serverless, databases, and developer
-          tools.
-        </H1>
-        <div className="flex flex-wrap justify-start gap-3 mt-0">
-          {session?.user?.username ? <TicketButton /> : <RegisterWithGitHub />}
+        <div className="my-auto max-w-[1280px] mx-auto flex flex-col items-center gap-16 p-4 leading-none text-center">
+          <video
+            autoPlay
+            onLoadStart={(e) => {
+              e.currentTarget.playbackRate = 1.5
+            }}
+            playsInline
+            muted
+            controls={false}
+            loop={false}
+            className="h-40 md:h-80"
+          >
+            <source src="/hero/fly-in.mov" type='video/mp4; codecs="hvc1"' />
+            <source src="/hero/fly-in.webm" type='video/webm; codecs="vp8"' />
+          </video>
+          <H1>
+            A free online event celebrating serverless, databases, and developer
+            tools.
+          </H1>
+          <div className="flex flex-wrap justify-start gap-3 mt-0">
+            {session?.user?.username ? (
+              <TicketButton />
+            ) : (
+              <RegisterWithGitHub />
+            )}
+          </div>
         </div>
       </div>
     </div>
