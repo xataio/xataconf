8c6da486ddcbc5928de6673a3864e1fd59228525
Tejas Kumar <tejas@tejas.qa>
2022-07-13 07:12:06 +0200

    Log errors

--- pages/api/og/index.ts
+++ pages/api/og/index.ts
@@ -22,9 +22,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
         res.end(file);
     } catch (e) {
+        console.error(e);
         res.statusCode = 500;
         res.setHeader('Content-Type', 'text/html');
         res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>');
-
     }
 }
